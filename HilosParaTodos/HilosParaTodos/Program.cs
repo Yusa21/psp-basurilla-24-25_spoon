f0196ab1c84ea4486f8543756e81d579c407ce15
Yusa21 <157543456+Yusa21@users.noreply.github.com>
2025-01-21 19:03:24 +0100

    Tarea, ahora si

--- HilosParaTodos/HilosParaTodos/Program.cs
+++ HilosParaTodos/HilosParaTodos/Program.cs
@@ -10,20 +10,3 @@ finalizarWrapper.Value += () => { Console.WriteLine("Suscriptor C"); };
 
 t1.Start();
 t2.Start();
-
-class Program 
-{
-    static void Main(string[] args)
-    {
-        var finalizarWrapper = new EventWrapper<Action>(() => { Console.WriteLine("Suscriptor A"); });
-        finalizarWrapper.Value += () => { Console.WriteLine("Suscriptor B"); };
-
-        MiHilo t1 = new MiHilo("x", finalizarWrapper);
-        MiHilo t2 = new MiHilo("y", finalizarWrapper);
-
-        finalizarWrapper.Value += () => { Console.WriteLine("Suscriptor C"); };
-
-        t1.Start();
-        t2.Start();
-    }
-}
